7fcd2320a0c3d6ba1f558e04e8706454f0b702dd
Agus Ibacache <chocobomj@gmail.com>
2022-05-05 20:00:18 -0300

    deleting the first and second prototype to begin with the third new prototype

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-let number1 = '';
-let storeNumber = document.querySelector("#btn");
-storeNumber.addEventListener('click' , function(){
-   let testing = '1';
-   number1 += testing;
-});
-let operate = function() {
+// mid prototype of the btm values
+let valueA = '';
+let extraA = ''; 
+let extraB = '';
 
 
-    let number1 = prompt("please,select a number")
-    let number2 = prompt("PLESEEEEE , number again")
-    let operator = prompt('you want to sum, mul, divide, or rest?');
-    if(operator == "+" ){
-        return sum(parseInt(number1),parseInt(number2))
-    }
-    else if(operator == "-"){
-        return res(number1,number2)
-    }
+
+
+const container = document.getElementById("container");
+for (let i = 0 ; i < 10 ; i++){
+    let btnCreate = document.createElement('button')
+    container.appendChild(btnCreate).className = i;
+    btnCreate.textContent= i;
+btnCreate.addEventListener ('click', () => {
+    valueA += btnCreate.className;
+    alert(valueA);
+})
 }
+//There are the values of every operator button and operator itself
+
+let operator = '';
+
+let SelOpPlus = document.querySelector("#plus");
+SelOpPlus.addEventListener('click', () => {
+    operator = '+';
+    alert(operator);
+    if(operator === '+' || operator === '-' || operator === '*' || operator === '/'){
+        extraA = parseInt(valueA);
+        valueA = '';
+    }
+})
+let SelOpMin = document.querySelector("#minus");
+SelOpMin.addEventListener('click', () => {
+    operator = '-';
+    alert(operator);
+})
+
+let SelOpMul = document.querySelector("#multiply");
+SelOpMul.addEventListener('click', () => {
+    operator = '*';
+    alert(operator);
+})
+
+let SelOpDiv = document.querySelector("#divide");
+SelOpDiv.addEventListener('click', () => {
+    operator = '/';
+    alert(operator);
+})
+
+
 
 
 // This are the operators, it needs an adjusment to work in events
@@ -36,3 +67,7 @@ let div = function(valueA, valueB){
     let result = valueA / valueB;
     return result
 }
+//early prototype of display
+let displayNum1 = document.querySelector("#number1");
+let displayNum2 = document.querySelector("#number2");
+let displayTotal = document.querySelector("#total");
